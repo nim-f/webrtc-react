ed6733c51a4c7b18b1b0519af3db0280dfb322ec
Анна Шахбазян <fatamorgana9205@gmail.com>
2022-02-20 18:30:17 +0200

    2-connect to WS

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+
+const WS = "http://localhost:8080";
+
+function App() {
+    useEffect(() => {
+        socketIOClient(WS);
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,28 @@
-console.log("it works!!!!");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors);
+const port = 8080;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
